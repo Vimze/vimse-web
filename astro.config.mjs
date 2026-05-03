53fbde2a153a8bf461b53bb2f30948c1823bc031
Anders Søbakken <anders.sobakken@gmail.com>
2026-05-03 15:36:57 +0200

    chore: Fix setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
+import sitemap from '@astrojs/sitemap';
+
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'static',
+  site: "https://nesh.no",
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  integrations: [sitemap()],
+  adapter: vercel(),
+});
